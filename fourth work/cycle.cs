a4b7464b8eb844ea9b70f22a3bebed6a906b2ef5
Kirill <112951242+KaVoshnik@users.noreply.github.com>
2024-04-02 21:16:03 +0700

    Add files via upload

---
+++ fourth work/cycle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace fourth{
+
+    class cycle{
+
+        static void Main(){
+
+            bool IsHasCar = false;
+            byte o = 100;
+
+            for(byte i = 0; i <= 10; i++) {
+                if(i % 2 == 0)
+                continue; // пропускает 1 элемент
+
+                if(i > 5)
+                    break; // брейк ну а че)
+                Console.WriteLine("Element: " + i);
+                // фигурные скобки при одной строке не прописывать
+            }
+
+            while(!IsHasCar){
+                string end = Console.ReadLine();
+
+                if(end == "end")
+                IsHasCar = true;
+            }
+
+            do {
+                Console.WriteLine("Element: " + o);
+            } while(o < 10);
+
+        }
+
+    }
+
+}
