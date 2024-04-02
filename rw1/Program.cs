f4b3e0e910245fdc86a42c14dea524835f2dc094
Kirill <112951242+KaVoshnik@users.noreply.github.com>
2024-04-02 21:15:07 +0700

    Add files via upload

---
+++ rw1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace rw1{
+
+    class rw1{
+
+        static void Main(){
+
+        for (int i = 100; i <= 200; i++) {
+            if(i % 3 == 0)
+            Console.WriteLine(i);
+
+        }
+
+        }
+
+    }
+
+}
+
+// Вывести на экран все целые числа от 100 до 200, кратные трем.
